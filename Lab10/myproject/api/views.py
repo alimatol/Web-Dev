362cf5eb2dcabb91c164956230a0f49c0624d44c
Alima <example@mail.com>
2026-04-05 22:19:14 +0500

    Lab10 commit

---
+++ Lab10/myproject/api/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Product, Category
+from django.http import JsonResponse
+
+
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .serializers import CategorySerializer, ProductSerializer
+# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def products(self, request, pk=None):
+        category = self.get_object()
+        products = Product.objects.filter(category=category)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
